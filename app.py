23b3a57dc6c5a5c459f98770172b3b3349da4d85
antoniobogaz <antonioaugustobogaz@hotmail.com>
2021-11-24 21:51:29 -0300

    teste

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 tarefas = [
-    {"texto": "Estudar para a prova", "concluida": False},
+    {"texto": "Estudar para a avaliação", "concluida": False},
     {"texto": "Passear com o cachorro", "concluida": True},
     {"texto": "Estudar mais para a prova", "concluida": False},
 ]
